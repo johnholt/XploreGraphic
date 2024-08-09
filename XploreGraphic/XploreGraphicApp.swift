e737fecd32a00b726aec3489688edd67d0513951
John Holt <jdholt77@earthlink.net>
2024-08-09 16:12:15 -0400

    Initial Commit

---
+++ XploreGraphic/XploreGraphicApp.swift
@@ -0,0 +1,17 @@
+//
+//  XploreGraphicApp.swift
+//  XploreGraphic
+//
+//  Created by John Holt on 8/9/24.
+//
+
+import SwiftUI
+
+@main
+struct XploreGraphicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ XploreGraphicUITests/XploreGraphicUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  XploreGraphicUITestsLaunchTests.swift
+//  XploreGraphicUITests
+//
+//  Created by John Holt on 8/9/24.
+//
+
+import XCTest
+
+final class XploreGraphicUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
